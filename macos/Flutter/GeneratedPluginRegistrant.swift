f4e5e42342e7283513a7e320224be0ac2fd7b943
sun30812 <sun30812@naver.com>
2022-02-08 21:37:46 +0900

    일부 기능 제거 및 요일 표시 추가
    1. 인터넷 연결상태를 확인하는 기능이 일부 호환되지 않는 경우가 있어 제거되었습니다.
    2. 이제 개설 강죄 조회할 때 강의실과 요일도 같이 출력됩니다.
    3. 자동화 파일은 나중에 앱 버전으로 빌드하기 위해 사용됩니다.

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cloud_firestore
+import connectivity_plus_macos
+import firebase_core
+import firebase_database
+import shared_preferences_macos
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
